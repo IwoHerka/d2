659588a3c4141a2d1415e636da3d4258fe026cfa
Iwo Herka <iwo.herka@makimo.pl>
2023-03-20 21:18:10 +0100

    Persist theme state

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function toggle_theme() {
-    document.body.classList.toggle("dark-theme");
+function toggleDarkTheme() {
+  if (localStorage.getItem('dark-theme') === 'enabled') {
+    localStorage.setItem('dark-theme', 'disabled');
+    document.body.classList.remove("dark-theme");
+  } else {
+    localStorage.setItem('dark-theme', 'enabled');
+    document.body.classList.add("dark-theme");
+  }
 }
+
+function loadThemeFromLocalStorage() {
+  if (localStorage.getItem('dark-theme') === 'enabled') {
+    document.body.classList.add("dark-theme");
+  }
+}
+
+document.addEventListener('DOMContentLoaded', loadThemeFromLocalStorage);
